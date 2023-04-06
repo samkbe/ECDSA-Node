4d062539476ed32e308f37980eabe1150ccdfd6e
Samkbe <samkbe@gmail.com>
2023-04-06 11:32:52 -0700

    Removed Console.logs

--- server/createKeys.js
+++ server/createKeys.js
@@ -1,6 +1,8 @@
 const { utils, getPublicKey } = require("ethereum-cryptography/secp256k1");
 const { toHex } = require("ethereum-cryptography/utils");
 
+// Use this to produce more wallets if needed.
+
 const privateKey = toHex(utils.randomPrivateKey());
 const publicKey = toHex(getPublicKey(privateKey));
 
